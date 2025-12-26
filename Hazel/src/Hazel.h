dcd803df8379521574b9c560fbee2b4884df3f8d
libantain <libantain@noreply.gitcode.com>
2025-12-26 10:49:10 +0800

    new

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -1,4 +1,11 @@
 #pragma once
 
+// For use by Hazel applications
+
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
+#include "Hazel/Layer.h"
+#include "Hazel/ImGui/ImGuiLayer.h"
+// ---Entry Point---------------------
 #include "Hazel/EntryPoint.h"
+// -----------------------------------
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,13 +1,37 @@
-#include <Hazel.h>
+#pragma once
+#include<Hazel.h>
 
-class Sandbox :public Hazel::Application
+
+class ExampleLayer : public Hazel::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example")
+	{
+	}
+
+	void OnUpdate() override
+	{
+		HZ_INFO("ExampleLayer::Update");
+	}
+
+	void OnEvent(Hazel::Event& event) override
+	{
+		HZ_TRACE("{0}", event.ToString());
+	}
+
+};
+
+class Sandbox : public Hazel::Application 
 {
 public:
 	Sandbox()
 	{
-
+		PushLayer(new ExampleLayer());
+		PushOverlay(new Hazel::ImGuiLayer());
 	}
-	~Sandbox()
+
+	~Sandbox() 
 	{
 
 	}
